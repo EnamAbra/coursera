b7b671c4be6734e8f0cec2af1c7eb016d65067d7
EnamAbra <ebene4321@gmail.com>
2024-09-15 11:47:03 -0400

    controlflow

--- Scope/scope_labs.js
+++ Scope/scope_labs.js
@@ -23,6 +23,14 @@ let functionLet = "I'm a block-scoped let";
 const functionConst = "I'm a block-scoped const";
 }
 show();
-console.log(functionVar); // Throws ReferenceError
-console.log(functionLet); // Throws ReferenceError
-console.log(functionConst); // Throws ReferenceError
+//console.log(functionVar); // Throws ReferenceError
+//console.log(functionLet); // Throws ReferenceError
+//console.log(functionConst); // Throws ReferenceError
+{
+let x = 8
+const pi = 3.17;
+var me = "enam"
+x=8
+pi = 7
+me = neme
+}
